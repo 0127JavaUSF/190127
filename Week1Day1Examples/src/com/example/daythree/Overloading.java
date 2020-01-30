d7316b005365981a0553a3ca732571f5cac47d03
clcollins1 <chrislcollins12@gmail.com>
2020-01-30 09:09:57 -0500

    changes

--- Week1Day1Examples/src/com/example/daythree/Overloading.java
+++ Week1Day1Examples/src/com/example/daythree/Overloading.java
@@ -4,19 +4,23 @@ public class Overloading {
 
 	public static void main(String[] args) {
 
-		System.out.println(add(5, 12));
+		//System.out.println(add(5, 12));
 		
-		System.out.println(add(1, 5, 34, 84, 9872));
+		//System.out.println(add(1, 5, 34, 84, 9872));
 		
-		System.out.println(add(3, "number?", 4, 5, 2, 4, 6, 3, 4334, 64, 234, 6543, 234, 65432, 65243, 32452, 652354, 2345, 42354, 6532));
+		//System.out.println(add(3, "number?", 4, 5, 2, 4, 6, 3, 4334, 64, 234, 6543, 234, 65432, 65243, 32452, 652354, 2345, 42354, 6532));
 		
 		//Wrapper Classes - Class used to wrap primative values
 		//Double, Short, Float, Long, Integer, Char, Boolean, Byte
 		Integer integer = new Integer(7);
+		Integer integer2 = new Integer(8);
+		Double doubl = new Double(5.0);
+		Double doubl2 = new Double(4.5);
 		//auto-unboxing: take a wrapper class and turn it into a primative type automatically
-		add(6, integer);
+		System.out.println(add(integer, 4));
+		System.out.println(add(4, doubl2));
 		//auto-boxing: take a primative and wrap it into the wrapper class automatically when Java expects a Class
-		takeInteger(5);
+		//takeInteger(5);
 	}
 	
 	public static int takeInteger(Integer i) {
@@ -24,18 +28,23 @@ public class Overloading {
 	}
 	
 	public static double add(double d, double d2) {
+		System.out.println("prints primitiave arg double");
 		return d + d2;
 	}
 	
 	public static double add(Double d, Double d2) {
+		System.out.println("prints wrapper arg double");
 		return d + d2;
 	}
 	
 	public static int add(Integer x, Integer y) {
+		System.out.println("prints wrapper arg int");
 		return x + y;
 	}
 	
+	
 	public static int add(int x, int y) {
+		System.out.println("prints primitave arg int");
 		return x + y;
 	}
 	
@@ -44,6 +53,7 @@ public class Overloading {
 		for (int i: s) {
 			result += i;
 		}
+		System.out.println("prints var arg");
 		return result;
 	}
 	
