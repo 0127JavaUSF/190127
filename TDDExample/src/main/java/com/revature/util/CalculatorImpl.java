d42664cd76dfef5edf00d975f622853517c6f735
jurczakn <nickolas.jurczak@gmail.com>
2020-01-30 15:06:53 -0500

    setup maven TDD

---
+++ TDDExample/src/main/java/com/revature/util/CalculatorImpl.java
@@ -0,0 +1,41 @@
+package com.revature.util;
+
+public class CalculatorImpl <T extends Number> implements Calculator<T> {
+
+	@Override
+	public T add(T a, T b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T sub(T a, T b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T mult(T a, T b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T div(T a, T b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T sqrt(T a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T pow(T a, T b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
