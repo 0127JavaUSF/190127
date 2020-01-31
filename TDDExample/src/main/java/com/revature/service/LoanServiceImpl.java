ce30436907d032474743cddc26f07c5b60fdff5b
jurczakn <nickolas.jurczak@gmail.com>
2020-01-31 11:00:26 -0500

    Mockito

---
+++ TDDExample/src/main/java/com/revature/service/LoanServiceImpl.java
@@ -0,0 +1,17 @@
+package com.revature.service;
+
+import com.revature.pojo.Loan;
+
+public class LoanServiceImpl implements LoanService {
+
+	@Override
+	public Double calculatePaymentAmount(Loan loan) {
+		return null;
+	}
+
+	@Override
+	public Double makeScheduledPayment(Loan loan) {
+		return null;
+	}
+
+}
